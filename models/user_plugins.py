8e7cd702ba151377a96a546d72ca2c4eafabb492
/Viresh <asviresh@gmail.com>
2013-12-12 15:00:55 +0530

    more changes to models

--- models/user_plugins.py
+++ models/user_plugins.py
@@ -1,33 +1,16 @@
-import cherrypy
-import sqlite3
-from jinja2 import Environment, FileSystemLoader
-import json
-
-def connect(thread_index):
-    cherrypy.thread_data.db = sqlite3.connect("users.db")
-
-cherrypy.engine.subscribe('start_thread', connect)
+from models.db_helper import DbHelper
 
 class UserPlugins:
+    def __init__(self):
+        self.db = DbHelper()
+        self.table = "user_plugins"
+
     def get_user_and_ph_num(self):
-        return self.select("user,ph_num")
+        return self.db.select(self.table, "user,ph_num")
 
     def plugins_subscribed_by(self, user):
-        return self.select("plugins", "user", user)
+        return self.db.select(self.table, "plugins", "user", user)
 
     def get_users(self):
-        return self.select("user")
-
-    def select(self, params='*', where='', where_params=''):
-        con = cherrypy.thread_data.db.cursor()
-        if where:
-            where_params = tuple(where_params.split(","))
-            where = "where %s = ?" % where
-            query = "select %s from user_plugins %s" % (params, where)
-            users = con.execute(query, where_params).fetchall()
-        else:
-            query = "select %s from user_plugins" % params
-            users = con.execute(query).fetchall()
-
-        return users
+        return self.db.select(self.table, "user")
 
