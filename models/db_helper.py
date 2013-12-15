09acdcacb870586b41c990b1e3448195026cc0ac
/Viresh <asviresh@gmail.com>
2013-12-15 14:59:44 +0530

    adding a db helper

---
+++ models/db_helper.py
@@ -0,0 +1,25 @@
+import cherrypy
+import sqlite3
+from jinja2 import Environment, FileSystemLoader
+import json
+
+def connect(thread_index):
+    cherrypy.thread_data.db = sqlite3.connect("users.db")
+
+cherrypy.engine.subscribe('start_thread', connect)
+
+class DbHelper:
+    def select(self, table, params='*', where='', where_params=''):
+        con = cherrypy.thread_data.db.cursor()
+        if where:
+            where_params = tuple(where_params.split(","))
+            where = "where %s = ?" % where
+            query = "select %s from %s %s" % (params, table, where)
+            print query
+            response = con.execute(query, where_params).fetchall()
+        else:
+            query = "select %s from %s" % (params, table)
+            response = con.execute(query).fetchall()
+
+        return response
+
