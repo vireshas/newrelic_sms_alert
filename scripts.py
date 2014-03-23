730ec76ddcf4c9f0f00f631436803f2724511ecc
Viresh <asviresh@gmail.com>
2014-03-23 12:21:17 +0530

    adding a script to pull new policies

--- scripts.py
+++ scripts.py
@@ -2,14 +2,17 @@
 import sys
 
 usage = "Usage:\npython scripts.py (option)\n\nOptions:\n  pull_new_users: "\
-        + "pull new users from newrelic\n  configure_db: creates table,"\
-        + "configures dbwith initial values"
+        + "pull new users from newrelic\n  configure_db: creates tables,"\
+        + "configures dbwith initial values"\
+        + "pull_new_plugins: pulls new plugins"
 
 if len(sys.argv) > 1:
     if sys.argv[1] == "pull_new_users":
         import scripts.pull_new_user
     elif sys.argv[1] == "configure_db":
         import scripts.db_init
+    elif sys.argv[1] == "pull_new_plugins":
+        import scripts.pull_new_plugins
     else:
         print usage
 else:
