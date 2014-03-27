66f576c3fbf1be08dc1ba848df2179be395a53db
Viresh <asviresh@gmail.com>
2014-03-27 12:21:50 +0530

    changing scripts.py

--- scripts.py
+++ scripts.py
@@ -4,14 +4,14 @@ import sys
 usage = "Usage:\npython scripts.py (option)\n\nOptions:\n  pull_new_users: "\
         + "pull new users from newrelic\n  configure_db: creates tables,"\
         + "configures dbwith initial values"\
-        + "pull_new_plugins: pulls new plugins"
+        + "pull_new_policies: pulls new policies"
 
 if len(sys.argv) > 1:
     if sys.argv[1] == "pull_new_users":
         import scripts.pull_new_user
     elif sys.argv[1] == "configure_db":
         import scripts.db_init
-    elif sys.argv[1] == "pull_new_plugins":
+    elif sys.argv[1] == "pull_new_policies":
         import scripts.pull_new_plugins
     else:
         print usage
